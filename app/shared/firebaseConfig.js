e522797d5dc498c6b5d4867fed723472f413b7e7
bxpxi <155725673+bxpxi@users.noreply.github.com>
2025-02-17 21:54:39 +0200

    Create firebaseConfig.js

---
+++ app/shared/firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD0gW03ZPGtx_EjJSXoT2G3EtSTZRadMiQ",
+  authDomain: "pinterest-clone-d83e1.firebaseapp.com",
+  projectId: "pinterest-clone-d83e1",
+  storageBucket: "pinterest-clone-d83e1.firebasestorage.app",
+  messagingSenderId: "553034200511",
+  appId: "1:553034200511:web:068742fbc4ec6063549d0a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app
